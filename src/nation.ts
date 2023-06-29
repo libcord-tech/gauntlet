c1af1698ac959731aecb4797a40777f536f4fa6f
Haku <123256704+paulhaku@users.noreply.github.com>
2023-06-29 11:00:04 -0400

    Remove useless log
    Co-authored-by: Pronoun <96925249+esfalsa@users.noreply.github.com>

--- src/nation.ts
+++ src/nation.ts
@@ -1,6 +1,5 @@
 (async () => {
     const autoScrollEnabled = await getStorageValue("scrollToBottom") ?? false;
-    console.log(autoScrollEnabled);
 
     if (autoScrollEnabled) {
         window.scrollTo(0, document.body.scrollHeight);
